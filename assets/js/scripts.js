b7ac70b64af800c46d40c957875f7a9b08a1320e
nodirbekeshnazarov7 <nodirbekeshnazarov7@gmail.com>
2025-04-13 21:34:51 +0500

    Desktop versiya boldi

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,70 @@
 $(document).ready(function () {
+// Modal ochish
+$('.open_custom_modal').on('click', function (e) {
+    e.preventDefault();
+    var modal_id = $(this).data('bs-target');
+    $(modal_id).fadeIn(200);
+    $('body').css('overflow', 'hidden');
+  });
+
+  // Modal yopish
+  $(document).on('click', '.custom_modal_close, .custom_modal_backdrop', function () {
+    $(this).closest('.custom_modal').fadeOut(200);
+    $('body').css('overflow', '');
+  });
+
+  // Form submitni bloklab, validatsiya qilish
+  $('.custom_modal form').on('submit', function (e) {
+    e.preventDefault(); // Formani yuborishni to‘xtatamiz
+
+    var $form = $(this);
+    var $inputs = $form.find('input[type="text"], input[type="tel"]');
+    var $checkbox = $form.find('input[type="checkbox"]');
+    var is_valid = true;
+
+    // Inputlar tekshiriladi
+    $inputs.each(function () {
+      if (!$(this).val().trim()) {
+        $(this).addClass('input_error');
+        is_valid = false;
+      } else {
+        $(this).removeClass('input_error');
+      }
+    });
+
+    // Checkbox tekshiriladi
+    if (!$checkbox.prop('checked')) {
+      $checkbox.addClass('input_error');
+      is_valid = false;
+    } else {
+      $checkbox.removeClass('input_error');
+    }
+
+    if (is_valid) {
+      // Hozirgi modalni yopamiz
+      $form.closest('.custom_modal').fadeOut(200);
+      $('body').css('overflow', 'hidden');
+
+      // 300ms kutamiz, keyin confirm_modalni ochamiz
+      setTimeout(function () {
+        $('#confirm_modal').fadeIn(200);
+      }, 300);
+    }
+  });
+
+  // A tugmalar o‘rniga form submit qilish
+  $('.custom_modal .credit_button a').on('click', function (e) {
+    e.preventDefault();
+    $(this).closest('form').submit();
+  });
 
     /** -------------------------------
      *  TABS
      * --------------------------------*/
     const $tabsContainer = $('.tabs_block');
+      
+
+
     if ($tabsContainer.length) {
         const $tabsWrapper = $tabsContainer.find('.tabs');
         const $tabs = $tabsWrapper.find('li');
@@ -156,5 +217,73 @@ $(document).ready(function () {
     $("select.custom-select-original").each(function () {
         createCustomSelect($(this));
     });
+
+
+    // Equipment card show more
+    const $equipmentItems = $('.equipment_card');
+    const $equipmentButton = $('.equipment_more a');
+    const $equipmentButtonText = $('.equipment_more span');
+    const $equipmentButtonImg = $('.equipment_more img');
+    const equipmentItemsToShow = 3;
+
+    let isExpanded = false;
+
+    function showLimitedItems() {
+        $equipmentItems.slice(equipmentItemsToShow).slideUp(); // faqat ortiqchasini yashir
+        $equipmentButtonText.text('Показать еще');
+        $equipmentButtonImg.removeClass('rotated');
+        isExpanded = false;
+    }
+
+    function showAllItems() {
+        $equipmentItems.slideDown();
+        $equipmentButtonText.text('Скрыть');
+        $equipmentButtonImg.addClass('rotated');
+        isExpanded = true;
+    }
+
+    // Boshlanish holati
+    $equipmentItems.hide();
+    $equipmentItems.slice(0, equipmentItemsToShow).show();
+
+    $equipmentButton.on('click', function (e) {
+        e.preventDefault();
+
+        if (isExpanded) {
+            showLimitedItems();
+        } else {
+            showAllItems();
+        }
+    });
+
+    // HTML classlar bilan moslashtirish
+    const footerTextElement = document.querySelector('.footer_desc p');
+    const toggleTextLink = document.querySelector('.footer_link .show_more');
+
+    // To'liq matnni olish
+    const fullText = footerTextElement.textContent.trim();
+
+    // Maksimal so'zlar soni
+    const maxWords = 30;
+    const words = fullText.split(" ");
+    const shortText = words.slice(0, maxWords).join(" ") + "...";
+
+    // Boshlang'ich matnni qisqartirish
+    let isFooterExpanded = false;
+    footerTextElement.textContent = shortText;
+
+    toggleTextLink.addEventListener("click", function (e) {
+        e.preventDefault();  // Linkning normal xatti-harakatini oldini olish
+        isFooterExpanded = !isFooterExpanded;
+
+        if (isFooterExpanded) {
+            footerTextElement.textContent = fullText;
+            toggleTextLink.textContent = "Скрыть";
+        } else {
+            footerTextElement.textContent = shortText;
+            toggleTextLink.textContent = "Подробнее";
+        }
+    });
+
 
 });
